8012c0eab007f3df6346bae898ab0d584c6f9683
Celso Silvestre <clsoft.silvestre@gmail.com>
2024-10-24 18:10:08 +0000

    Showing id of sub elements

--- src/app/components/modal-details/modal-details.component.ts
+++ src/app/components/modal-details/modal-details.component.ts
@@ -1,10 +1,13 @@
-import { Component, Inject, OnInit  } from '@angular/core';
+import { Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { SiomeApiProviderService } from '../../services/siome-api-provider.service';
 import { ISiomeApi } from '../../shared/public-api/interfaces/siome-api.interface';
 import { NodeClass } from 'src/app/shared/public-api/enums/node-classes';
 import { IAddObjectParameter } from 'src/app/shared/public-api/interfaces/add-object-parameter';
+import { INodeId } from 'src/app/shared/public-api/opcua-types/interfaces/node-id.interface';
+import { IOpcNode } from 'src/app/shared/public-api/interfaces/opc-node.interface';
+
 
 @Component({
   selector: 'app-modal-details',
@@ -26,6 +29,16 @@ export class ModalDetailsComponent implements OnInit {
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  async getNode(namespace:number, value:number) {
+    let id = "ns=" + String(namespace) + ";i=" + String(value);
+    //let element = await this.siomeApi.getOpcNode(id);
+    await this.siomeApi.createLogNode("Bel Plugin - Debug");
+    await this.siomeApi.newLogEntry("Browsing ID " + id + ".", "info");
+
+    //return (await this.siomeApi.getOpcNode(id))
+    return "OK";
   }
 
 }
